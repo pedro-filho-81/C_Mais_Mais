0f78de4ba6bb5a1953a9cb63ba1ceb5b8422b0dd
PedroFilho <psfilho81@hotmail.com>
2021-08-04 11:37:35 -0300

    Update Imprime4numeros.cpp

--- Capitulo02/Exercicios/Imprime4numeros.cpp
+++ Capitulo02/Exercicios/Imprime4numeros.cpp
@@ -19,10 +19,15 @@ int main()
     // define a localização geográfica
     setlocale(LC_ALL, "portuguese");
 
+    // 2.17 Escreva um programa que imprime os números 1 a 4 na mesma linha
+
+    // a) Utilizando uma instrução com um operador de inserção de fluxo.
     cout << "1 2 3 4\n";
 
+    // b) Utilizando uma instrução com quatro operadores de inserção de fluxo.
     cout << "1 " << "2 " << "3 " << "4\n";
 
+    // c) Utilizando quatro instruções.
     cout << "1 ";
     cout << "2 ";
     cout << "3 ";
