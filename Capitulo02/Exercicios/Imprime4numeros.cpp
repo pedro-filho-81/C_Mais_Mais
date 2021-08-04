7ae40ebb3e77d28048e0811f2f6a4218f3abc9ac
PedroFilho <psfilho81@hotmail.com>
2021-08-04 10:39:37 -0300

    Imprimindo 4 Números

---
+++ Capitulo02/Exercicios/Imprime4numeros.cpp
@@ -0,0 +1,33 @@
+/*
+    2.17 Escreva um programa que imprime os números 1 a 4 na mesma linha com
+    cada par de números adjacentes separados por um espaço. Faça  isso de várias maneiras:
+    a) Utilizando uma instrução com um operador de inserção de fluxo.
+    b) Utilizando uma instrução com quatro operadores de inserção de fluxo.
+    c) Utilizando quatro instruções.
+    Deitel, Harvey; Paul, Harvey. C++ Como Programar (p. 53). Edição do Kindle.
+    Autor:
+*/
+
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+// função principal
+int main()
+{
+    // define a localização geográfica
+    setlocale(LC_ALL, "portuguese");
+
+    cout << "1 2 3 4\n";
+
+    cout << "1 " << "2 " << "3 " << "4\n";
+
+    cout << "1 ";
+    cout << "2 ";
+    cout << "3 ";
+    cout << "4\n";
+
+    return 0; // programa terminado com sucesso
+
+} // fim main
