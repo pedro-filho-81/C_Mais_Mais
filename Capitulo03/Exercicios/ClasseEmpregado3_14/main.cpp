01889e8f0cf21bb4c0a1a61d328af4108b7464bd
PedroFilho <psfilho81@hotmail.com>
2021-08-10 10:45:16 -0300

    Classe Empregado

---
+++ Capitulo03/Exercicios/ClasseEmpregado3_14/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // limpa a tela
+    system("cls");
+
+    // olá mundo
+    cout << "Hello world!" << endl;
+
+    // pausa o programa
+    system("pause");
+
+    // fum do programa
+    return 0;
+
+} // fim man
