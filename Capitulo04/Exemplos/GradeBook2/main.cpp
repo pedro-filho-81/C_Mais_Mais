47b1986898770307e1b33ec0d22f0a6b6e11434a
PedroFilho <psfilho81@hotmail.com>
2021-08-12 15:13:28 -0300

    Classe GradeBook2

---
+++ Capitulo04/Exemplos/GradeBook2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // limpa a tela
+    system("cls");
+
+    cout << "Hello world!" << endl;
+
+    // pausa
+    system("pause");
+
+    // fim do programa
+    return 0;
+} // fim main
