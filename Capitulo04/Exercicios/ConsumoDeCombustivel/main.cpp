6573c94a63cb75670f1dec87a5088d493e95fdee
PedroFilho <psfilho81@hotmail.com>
2021-08-14 11:59:34 -0300

    Função main da Classe Consumo

--- Capitulo04/Exercicios/ConsumoDeCombustivel/main.cpp
+++ Capitulo04/Exercicios/ConsumoDeCombustivel/main.cpp
@@ -1,27 +1,24 @@
-/*
-    4.13 Os motoristas se preocupam com o consumo de combustível dos seus automóveis.
-    Um motorista monitorou vários tanques cheios de  gasolina registrando a quilometragem
-    dirigida e a quantidade de combustível em litros utilizados para cada tanque cheio.
-    Desenvolva um  programa C++ que utiliza uma instrução while para inserir os quilômetros
-    percorridos e a quantidade de litros de gasolina utilizados  para cada taque.
-    O programa deve calcular e exibir o consumo em quilômetros/litro para cada tanque cheio
-    e imprimir a quilometragem  combinada e a soma total de litros de combustível consumidos
-    até esse ponto.
-    Deitel, Harvey; Paul, Harvey. C++ Como Programar (p. 133). Edição do Kindle.
-    Autor: Pedro Filho, 14/08/2021
-*/
+
 #include <iostream>
 #include <locale>
+
+#include "Consumo.h" // inclui a classe Consumo no programa principal
 
 using namespace std;
 
 int main()
 {
     // localização geográfica
-    setlocale( LC_ALL; "portuguese");
+    setlocale( LC_ALL, "portuguese");
 
     // limpa a tela
     system("cls");
+
+    // cria o objeto da classe Consumo
+    Consumo meuConsumo( 287, 13 );
+
+    // mostra o consumo
+    meuConsumo.consumoKmPorLitros( 287, 13 );
 
     // pausa o programa
     system("pause");
