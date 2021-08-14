232088e1ad1f3b55cd8dca551ae564f28a5a9f6b
PedroFilho <psfilho81@hotmail.com>
2021-08-14 10:54:30 -0300

    Consumo de Combustíveis
    4.13 Os motoristas se preocupam com o consumo de combustível dos seus automóveis.
    
    Deitel, Harvey; Paul, Harvey. C++ Como Programar (p. 133). Edição do Kindle.

---
+++ Capitulo04/Exercicios/ConsumoDeCombustivel/main.cpp
@@ -0,0 +1,31 @@
+/*
+    4.13 Os motoristas se preocupam com o consumo de combustível dos seus automóveis.
+    Um motorista monitorou vários tanques cheios de  gasolina registrando a quilometragem
+    dirigida e a quantidade de combustível em litros utilizados para cada tanque cheio.
+    Desenvolva um  programa C++ que utiliza uma instrução while para inserir os quilômetros
+    percorridos e a quantidade de litros de gasolina utilizados  para cada taque.
+    O programa deve calcular e exibir o consumo em quilômetros/litro para cada tanque cheio
+    e imprimir a quilometragem  combinada e a soma total de litros de combustível consumidos
+    até esse ponto.
+    Deitel, Harvey; Paul, Harvey. C++ Como Programar (p. 133). Edição do Kindle.
+    Autor: Pedro Filho, 14/08/2021
+*/
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    // localização geográfica
+    setlocale( LC_ALL; "portuguese");
+
+    // limpa a tela
+    system("cls");
+
+    // pausa o programa
+    system("pause");
+
+    // fim do programa
+    return 0;
+} // fim main
