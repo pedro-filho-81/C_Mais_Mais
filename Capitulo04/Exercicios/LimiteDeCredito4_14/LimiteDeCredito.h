522ba1201b6e2343b9d978e1c678e40f65d5374b
PedroFilho <psfilho81@hotmail.com>
2021-08-14 18:44:46 -0300

    Limite de Crédito
    Exercício 4.14 Desenvolva um programa C++ que determinará se um cliente de uma loja de departamentos excedeu o limite de crédito em uma conta  corrente.
    
    Deitel, Harvey; Paul, Harvey. C++ Como Programar (p. 134). Edição do Kindle.

---
+++ Capitulo04/Exercicios/LimiteDeCredito4_14/LimiteDeCredito.h
@@ -0,0 +1,11 @@
+// Cria a classe limite de crédito
+#include <iostream>
+
+using namespace std;
+
+// cria a classe
+class LimiteDeCredito
+{
+public:
+    void setConta( int ); // configura o número da conta
+}; // fim classe
---
+++ Capitulo04/Exercicios/LimiteDeCredito4_14/main.cpp
@@ -0,0 +1,20 @@
+// função principal main testa a classe LimiteDeCredito
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    // localização geografica
+    setlocale( LC_ALL, "portuguese" );
+
+    // limpa a tela
+    system("cls");
+
+    // pausa o programa
+    system("pause");
+
+    // fim do programa
+    return 0;
+} // fim de main
