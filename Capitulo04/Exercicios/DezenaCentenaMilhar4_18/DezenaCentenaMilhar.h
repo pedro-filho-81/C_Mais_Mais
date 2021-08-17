4ee4d77850b1fcb0dc3a98f5ae821bd94d01b72a
PedroFilho <psfilho81@hotmail.com>
2021-08-17 10:06:50 -0300

    Dezena Centena Milhar

---
+++ Capitulo04/Exercicios/DezenaCentenaMilhar4_18/DezenaCentenaMilhar.h
@@ -0,0 +1,15 @@
+// cria a classe DezenaCentenaMilhar
+#include <iostream>
+
+using namespace std;
+
+// cria classe
+class DezenaCentenaMilhar
+{
+public:
+    // protótipos de função
+    void mostrarDezCemMil();
+private:
+    // cria variáveis
+
+}; // fim classe
