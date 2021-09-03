2a04e60f3fafcf71a78582f83d619e6d42d52c13
PedroFilho <psfilho81@hotmail.com>
2021-09-03 11:17:21 -0300

    Grade Book

---
+++ Capitulo06/Exemplos/GradeBook6_3/main.cpp
@@ -0,0 +1,31 @@
+/*
+
+*/
+
+#include <iostream>
+#include <locale>
+#include "GradeBook6_3.h"
+
+using namespace std;
+
+int main()
+{
+    // localização
+    setlocale( LC_ALL, "portuguese" );
+
+    // limpa a tela
+    system("cls");
+
+    // variáveis
+
+
+    // pula uma linha
+    cout << endl;
+
+    // pausa o programa
+    system("pause");
+
+    // fim do programa
+    return 0;
+
+} // fim main
