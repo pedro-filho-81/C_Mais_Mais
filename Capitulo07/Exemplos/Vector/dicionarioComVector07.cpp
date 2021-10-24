e27eaa0996458750a9958ca29412d2c96b867b23
PedroFilho <psfilho81@hotmail.com>
2021-10-24 17:33:15 -0300

    Create dicionarioComVector07.cpp

---
+++ Capitulo07/Exemplos/Vector/dicionarioComVector07.cpp
@@ -0,0 +1,37 @@
+/*
+
+    Autor:
+*/
+
+#include <iostream>
+#include <locale>
+#include <vector>
+#include <string>
+
+// função principal
+int main()
+{
+    // localização geográfica
+    setlocale( LC_ALL, "portuguese");
+
+    // limpa a tela
+    system("cls");
+
+    vector<string> palavras; // cria o vetor palavras
+
+    for (string temp; cin >> temp;) // ler palavras separadas por espaços em branco
+        palavras.push_back (temp); // coloca palavras no vetor
+
+    cout << "Número de palavras:" << palavras.size () << '\ n';
+
+    sort( palavras.begin(), palavras.end() ); // classifica as palavras
+
+    for(int i = 0; i < palavras.size (); ++ i)
+          if (i == 0 || palavras [ i ] != palavras [ i - 1 ]  ) // esta é uma palavra nova?
+            cout << palavras [i] << endl;
+
+    system("pause"); // pausa do programa
+
+    return 0; // programa terminado com sucesso
+
+} // fim main
