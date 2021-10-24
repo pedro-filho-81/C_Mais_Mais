d7c4a2788d7d2b0ed49e855718315cb6fb78ac7c
PedroFilho <psfilho81@hotmail.com>
2021-10-24 11:44:02 -0300

    Atualização

--- Capitulo07/Exemplos/Vector/adicionandoValoresAoVetor03.cpp
+++ Capitulo07/Exemplos/Vector/adicionandoValoresAoVetor03.cpp
@@ -3,9 +3,11 @@
     22/10/2021
 */
 
-#include <iostream>
-#include <locale>
-#include <vector>
+// biblioteca padrão
+#include <iostream> // cout, cin
+#include <locale> // setlocale
+#include <vector> // vector
+#include <algorithm> // sort
 
 using namespace std;
 
@@ -24,21 +26,60 @@ int main()
     // header
     cout << "ADICIONA VALORES COM PUSH_BACK" << endl;
 
+    // data input
     cout << "Digite qualquer número ( um caractere = sair ): " << endl;
 
     // loop para adicionar valores ao vetor
-    for( valor = 0; cin >> valor;)
+    for( double valor = 0; cin >> valor;)
     {
         // adiciona um elemento no final do vetor
         vetor.push_back(valor);
     } // endl for
 
-    // loop para percorrer os valores do vetor
+    cout << "\nvetor original = ";
+
+    // loop para mostrar os valores do vetor
     for( double valor : vetor )
         // imprime os valores do vetor
         cout << valor << " ";
 
     cout << endl; // next line
+
+    // organiza o vetor
+    sort( vetor.begin(), vetor.end() );
+
+    cout << "\nvetor organizado = ";
+
+    // loop para mostrar os valores do vetor
+    for( double valor : vetor )
+        // imprime os valores do vetor
+        cout << valor << " ";
+
+    cout << endl; // next line
+
+    // create variable
+    double soma = 0; // soma os valores do vetor
+    double maior = vetor[ 0 ]; // para o maior valor do vetor
+    double menor = vetor[ 0 ]; // para o menor valor do vetor
+
+    // loop pelos valores do vetor
+    for( double valor : vetor )
+    {
+        soma += valor; // soma os valores
+
+        if( valor > maior ) // se valor maior que maior
+            maior = valor; // maior recebe o valor
+
+        if ( valor < menor ) // se o valor for menor que menor
+            menor = valor;  // menor recebe o valor
+    } // fim for
+
+    // imprime resultado
+    cout << "\nSoma = " << soma // imprime a soma
+            << "\nMédia = " << soma / vetor.size() // imprime a média
+            << "\nMediana = " << vetor[ vetor.size() / 2 ] // imprime a mediana
+            << "\nMenor valor = " << menor // imprime o menor valor
+            << "\nMaior valor = " << maior << endl; // imprime o maior valor
 
     system("pause"); // pausa do programa
 
