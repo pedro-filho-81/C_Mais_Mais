86a2ca0703c68b667d8da8b1a15b729f5ee0f324
PedroFilho <psfilho81@hotmail.com>
2021-11-10 16:03:02 -0300

    Atualização

--- C++PrincipiosPraticas/Capitulo03/Exercicios/TresStringsEmOrdem07.cpp
+++ C++PrincipiosPraticas/Capitulo03/Exercicios/TresStringsEmOrdem07.cpp
@@ -21,7 +21,7 @@ int main()
     // variáveis
     string nome1, nome2, nome3;
 
-    cout << "STRINGS EM ORDEM" << endl;
+    cout << "STRINGS EM ORDEM ALFABÉTICA" << endl;
 
     // entrada de dados
     cout << "Digite 3 nomes de pessoas: ";
@@ -29,37 +29,37 @@ int main()
 
     if( nome1 < nome2 && nome2 < nome3 && nome3 > nome1 ) // 1 2 3
         cout << nome1 << ", "<< nome2 << ", "<< nome3 << endl;
-/*
-    if( val1 < val2 && val1 <  val3 && val3 < val2 ) // 1 3 2
-        cout << val1 << " " << val3 << " " << val2 << endl;
 
-    if( val2 < val1 && val2 < val3 && val1 < val3 ) // 2 1 3
-        cout << val2 << " " << val1 << " " << val3 << endl;
+    if( nome1 < nome2 && nome1 <  nome3 && nome3 < nome2 ) // 1 3 2
+        cout << nome1 << " " << nome3 << " " << nome2 << endl;
 
-   if( val1 < val2 && val3 < val1 && val3 < val2 ) // 2 3 1
-        cout << val3 << " " << val1 << " " << val2 << endl;
+    if( nome2 < nome1 && nome2 < nome3 && nome1 < nome3 ) // 2 1 3
+        cout << nome2 << " " << nome1 << " " << nome3 << endl;
 
-   if( val1 > val2 && val1 > val3 && val2 > val3 ) // 3 2 1
-        cout << val3 << " " << val2 << " " << val1 << endl;
+   if( nome1 < nome2 && nome3 < nome1 && nome3 < nome2 ) // 2 3 1
+        cout << nome3 << " " << nome1 << " " << nome2 << endl;
 
-   if( val1 > val2 && val1 > val3 && val2 < val3 ) // 3 1 2
-        cout << val2 << " " << val3 << " " << val1 << endl;
+   if( nome1 > nome2 && nome1 > nome3 && nome2 > nome3 ) // 3 2 1
+        cout << nome3 << " " << nome2 << " " << nome1 << endl;
 
-    if( val1 == val2 && val2 == val3  && val3 == val1 ) // todos iguais
-        cout << val1 << ", " << val2 << ", " << val3 << endl;
+   if( nome1 > nome2 && nome1 > nome3 && nome2 < nome3 ) // 3 1 2
+        cout << nome2 << " " << nome3 << " " << nome1 << endl;
 
-    if( val1 == val2 && val3 > val1  && val3 > val2 ) // 1 1 2
-        cout << val1 << ", " << val2 << ", " << val3 << endl;
+    if( nome1 == nome2 && nome2 == nome3  && nome3 == nome1 ) // todos iguais
+        cout << nome1 << ", " << nome2 << ", " << nome3 << endl;
 
-    if( val1 == val2 && val3 < val1  && val3 < val2 ) // 2 2 1
-        cout << val1 << ", " << val2 << ", " << val3 << endl;
+    if( nome1 == nome2 && nome3 > nome1  && nome3 > nome2 ) // 1 1 2
+        cout << nome1 << ", " << nome2 << ", " << nome3 << endl;
 
-    if( val1 < val2 && val1 < val3  && val2 == val3 ) // 1 2 2
-        cout << val2 << ", " << val3 << ", " << val1 << endl;
+    if( nome1 == nome2 && nome3 < nome1  && nome3 < nome2 ) // 2 2 1
+        cout << nome1 << ", " << nome2 << ", " << nome3 << endl;
 
-    if( val1 > val2 && val1 > val3  && val2 == val3 ) // 2 1 1
-        cout << val2 << ", " << val3 << ", " << val1 << endl;
-*/
+    if( nome1 < nome2 && nome1 < nome3  && nome2 == nome3 ) // 1 2 2
+        cout << nome2 << ", " << nome3 << ", " << nome1 << endl;
+
+    if( nome1 > nome2 && nome1 > nome3  && nome2 == nome3 ) // 2 1 1
+        cout << nome2 << ", " << nome3 << ", " << nome1 << endl;
+
     system("pause"); // pausa do programa
 
     return 0; // programa terminado com sucesso
