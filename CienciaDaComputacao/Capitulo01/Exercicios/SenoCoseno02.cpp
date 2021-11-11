6aab7943c6ee6bfef891529c2368b9fec26f5096
PedroFilho <psfilho81@hotmail.com>
2021-11-11 17:37:52 -0300

    Create SenoCoseno02.cpp

---
+++ CienciaDaComputacao/Capitulo01/Exercicios/SenoCoseno02.cpp
@@ -0,0 +1,27 @@
+/*
+    1.2.2 Escreva um programa que use Math.sin () e Math.cos () para verificar se
+    o valor de cos2 θ + sin2 θ é aproximadamente 1 para qualquer θ inserido como
+    um argumento de linha de comando. Basta imprimir o valor. Por que os valores
+    nem sempre são exatamente 1?
+    Autor: Pedro, 10/1/2021
+*/
+
+#include <iostream>
+#include <locale>
+#include <cmath>
+using namespace std;
+
+// função principal
+int main()
+{
+    // localização geográfica
+    setlocale( LC_ALL, "portuguese");
+
+    // limpa a tela
+    system("cls");
+
+    system("pause"); // pausa do programa
+
+    return 0; // programa terminado com sucesso
+
+} // fim main
