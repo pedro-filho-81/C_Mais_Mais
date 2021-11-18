85dfe441221941943bbc5720bcf03a788b99a894
PedroFilho <psfilho81@hotmail.com>
2021-11-18 17:22:27 -0300

    Atualização

--- ResumoC++/TutorialC++.cpp
+++ ResumoC++/TutorialC++.cpp
@@ -1,5 +1,6 @@
 /*
-    Criar um resumo da linguagem de programação C++ para auxiliar meu aprendizado.
+    Criar um resumo da linguagem de programação C++ para auxiliar o aprendizado.
+    os detalhes só serão vistos com a execução do programa
     Feito por: Pedro Filho,18/11/2021
 */
 
@@ -32,15 +33,22 @@ int main()
     switch( opcaoMenu )
     {
         case 1:
-            cout << "Primeiro programa em C++\n" << endl;
-            cout << "#include <iostream>" << endl;
-            cout << "using namespace std;\n" << endl;
-            cout << "int main ()" << endl;
-            cout << "{ // início do bloco em C++.\n" << endl;
-            cout << "    cout << \"Olá, Mundo C++!\" << endl;" << endl;
-            cout << "    return 0; // programa finalizado com sucesso\n" << endl;
-            cout << "} // fim da função principal (main() )" << endl;
+            cout << "\nPrimeiro programa em C++\n" << endl;
+            cout << "// => barra barra ==> significa comentário de uma linha.\n" << endl;
+            cout << "// OBRIGATÓRIO incluir biblioteca de entrada e saída\n" << endl;
+            cout << "\t#include <iostream> // OBRIGATÓRIO para poder utulizar os comandos cout e cin\n" << endl;
+            cout << "\tusing namespace std; // OBRIGATÓRIO para não precisar usar std::cout, std::cin, std::endl;\n" << endl;
+            cout << "\tint main () // OBRIGATÓRIO É a função principal em qualquer programa C++\n" << endl;
+            cout << "\t{ // OBRIGATÓRIO abre-chave é o início do bloco de execução da funçao main em C++.\n" << endl;
+            cout <<  "\n\t    // cout comando para mostrar a string Olá, Mundo C++ na tela." << endl;
+            cout << "\t    cout << \"Olá, Mundo C++!\" << endl;// ponto e virgúla marca o fim da instrução\n" << endl;
+            cout << "\t    return 0; // programa finalizado com sucesso\n" << endl;
+            cout << "\t} // OBRIGATÓRIO fecha-chave fim da função principal (main() )" << endl;
+            cout << "\tMemorize tudo que for obrigatório." << endl;
+            cout << "\nMensagem gerada pelo compilador após return = 0;\n"
+                    << "Process returned 0 (0x0)   execution time : 11.247 s" << endl;
             cout << endl; // pula uma linha
+
             break;
         case 2:
             cout << "Variável - Nome dado ao espaço da memória"
