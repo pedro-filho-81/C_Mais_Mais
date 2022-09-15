ee8ae54a6dcd5c5ef13643836accc3cae2569bb0
PedroFilho <psfilho81@hotmail.com>
2022-09-15 12:13:31 -0300

    Create ArrayEstatico11.cpp

---
+++ Capitulo07/Arrays/ArrayEstatico11.cpp
@@ -0,0 +1,21 @@
+/*
+    array estático são inicializados como zero
+*/
+#include <iostream>
+#include <clocale>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+
+
+    system("pause"); // pausa do programa
+
+    return 0;
+
+} // final principal
